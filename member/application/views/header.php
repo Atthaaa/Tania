41223b3e6815bac84028ec228a4ff64183192d2e
Vhemas07 <153825798+Vhemas07@users.noreply.github.com>
2024-12-02 18:28:48 +0700

    Tampilan Hama Penyakit
    Menampilkan tampilan info hama penyakit

--- member/application/views/header.php
+++ member/application/views/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>MEMBER MARKETPLACE</title>
+  <title>Tania</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
   <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
@@ -29,7 +29,7 @@
                 <a href="<?php echo base_url('') ?>" class="nav-link">Home</a>
                 </li>
                 <li class="dropdown-item bg-success">
-                  <a href="<?php echo base_url('hama_penyakit') ?>" class="nav-link">Hama Penyakit</a>
+                  <a href="<?php echo base_url('Hama_penyakit') ?>" class="nav-link">Hama Penyakit</a>
                 </li>
                 <li class="dropdown-item bg-success">
                   <a href="<?php echo base_url('layanan_keuangan') ?>" class="nav-link">Layanan Keuangan</a>
@@ -45,21 +45,6 @@
         </ul>
         <?php if ($this->session->userdata('id_member')) : ?>
           <ul class="navbar-nav ms-auto">
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Seller
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="<?php echo base_url('seller/produk') ?>">Produk Saya</a></li>
-                <li><a class="dropdown-item" href="<?php echo base_url('seller/transaksi') ?>">Penjualan Saya</a></li>
-                <li><a href="<?php echo base_url('transaksi') ?>" class="dropdown-item">Pembelian Saya</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a href="<?php echo base_url('akun') ?>" class="nav-link">
-                <?php echo $this->session->userdata('nama_member') ?>
-              </a>
-            </li>
             <li class="nav-item">
               <a href="<?php echo base_url('logout') ?>" class="nav-link">Logout</a>
             </li>
